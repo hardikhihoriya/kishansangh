bc2299b7973e96b5ac4d109945280d85eb518a24
Viral <viral85@gmail.com>
2018-02-19 19:19:39 +0530

    designs for dashboard.blade.php

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -12,30 +12,40 @@
 
 <section class="content">
     <div class="row">
-        <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
-            <h3>
-                <a href='admin/sanghusers' >
-                    {{ $totalRegistration }} {{trans('adminlabels.TOTAL_REGISTERATION')}}
-                </a>
-            </h3>
+        <div class="col-lg-4 col-xs-6">
+            <!-- small box -->
+            <div class="small-box bg-green">
+                <div class="inner" style="height: 93px;">
+                    <div>
+                        <h2 style="padding: 5px;">{{trans('adminlabels.TOTAL_REGISTERATION')}} : {{ $totalRegistration }} </h2> 
+                    </div>
+                </div>
+                <a href='admin/sanghusers' class="small-box-footer" style="padding: 5px; font-size: 20px;">{{trans('adminlabels.TOTAL_REGISTERATION')}}<i class="fa fa-users" style="padding-left: 10px;"></i></a>
+            </div>
         </div>
-        <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
-            <h3>
-                <a href='admin/customer' >
-                    {{ $customer }} {{trans('adminlabels.CUSTOMER')}}
-                </a>
-            </h3>
+        <div class="col-lg-4 col-xs-6">
+            <!-- small box -->
+            <div class="small-box bg-primary">
+                <div class="inner" style="height: 93px;">
+                    <div>
+                        <h2 style="padding: 5px;">{{trans('adminlabels.CUSTOMER')}} : {{ $customer }}</h2> 
+                    </div>
+                </div>
+                <a  href='admin/customer' class="small-box-footer" style="padding: 5px; font-size: 20px;">{{trans('adminlabels.CUSTOMER')}} <i class="fa fa-user" style="padding-left: 10px;"></i></a>
+            </div>
         </div>
-        <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
-            <h3>
-                <a href='admin/vendor' >
-                    {{ $vendor }} {{trans('adminlabels.VENDOR')}}
-                </a>
-            </h3>
-        </div>
-        <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
-            
-        </div>
+
+        <div class="col-lg-4 col-xs-6">
+            <!-- small box -->
+            <div class="small-box bg-teal">
+                <div class="inner" style="height: 93px;">
+                    <div>
+                        <h2 style="padding: 5px;">{{trans('adminlabels.VENDOR')}} : {{ $vendor }} </h2> 
+                    </div>
+                </div>
+                <a href='admin/vendor' class="small-box-footer" style="padding: 5px; font-size: 20px;">{{trans('adminlabels.VENDOR')}} <i class="fa fa-shopping-cart" style="padding-left: 10px;"></i></a>
+            </div>
+        </div>         
     </div>
 </section>
 @stop
